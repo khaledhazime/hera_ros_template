a6591939b1126b3413f7fcafbe9ad7ff80b5c3e6
Khaled Hazime <hazimekhaled@icloud.com>
2024-01-17 19:08:20 -0300

    Refactor ROSNodeTemplate class in example_node.py

--- src/example_node.py
+++ src/example_node.py
@@ -5,35 +5,30 @@ from hera_ros_template.msg import ExampleMessage
 from hera_ros_template.srv import ExampleService, ExampleServiceResponse
 
 class ROSNodeTemplate:
+    """A ROS node template for publishing, subscribing, and services."""
+
     def __init__(self):
-        # Initialize the ROS node
+        """Initializes the node, publisher, subscriber, and service."""
         rospy.init_node('ros_node_template')
 
-        # Publisher
         self.publisher = rospy.Publisher('example_message_topic', ExampleMessage, queue_size=10)
-
-        # Subscriber
         rospy.Subscriber('example_message_input', ExampleMessage, self.callback)
-
-        # Service
         self.service = rospy.Service('example_service', ExampleService, self.handle_service)
-
-        # Rate for the main loop
         self.rate = rospy.Rate(1)  # 1 Hz
 
     def callback(self, data):
-        # Subscriber callback function
+        """Processes received messages."""
         rospy.loginfo(f'Received message: id={data.id}, name={data.name}, value={data.value}')
 
     def handle_service(self, req):
-        # Service callback function
+        """Handles service requests."""
         response = ExampleServiceResponse()
         response.success = True
         response.message = f'Received in service: id={req.request_data.id}, name={req.request_data.name}, value={req.request_data.value}'
         return response
 
     def publish_example_message(self):
-        # Publishes an ExampleMessage
+        """Publishes an ExampleMessage."""
         message = ExampleMessage()
         message.id = 1
         message.name = "Test"
@@ -41,7 +36,7 @@ class ROSNodeTemplate:
         self.publisher.publish(message)
 
     def run(self):
-        # Main loop
+        """Main loop for the node."""
         while not rospy.is_shutdown():
             self.publish_example_message()
             self.rate.sleep()
